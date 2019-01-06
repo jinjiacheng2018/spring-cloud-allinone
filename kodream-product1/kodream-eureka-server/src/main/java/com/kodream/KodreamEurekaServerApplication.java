e994f2c567153ed38039be366c1b7479f1eee1f4
jinjiacheng2018 <1935922807@qq.com>
2019-01-06 12:13:36 +0800

    eureka服务端

--- kodream-product1/kodream-eureka-server/src/main/java/com/kodream/KodreamEurekaServerApplication.java
+++ kodream-product1/kodream-eureka-server/src/main/java/com/kodream/KodreamEurekaServerApplication.java
@@ -2,7 +2,17 @@ package com.kodream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ *<p> Description: Eureka的服务端启动类 </p>
+ *<p> Copyright: Copyright(c) 2019/1/6 </p>
+ *<p> Company: 7qb </p>
+ *
+ *@author JinJiacheng
+ *@Version 1.0 2019/1/6 12:10
+ */
+@EnableEurekaServer
 @SpringBootApplication
 public class KodreamEurekaServerApplication
 {
